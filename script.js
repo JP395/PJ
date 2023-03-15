87a239044d82a9ace65a14b20228764a3177d67d
JOHN PINTO <>
2023-03-15 13:33:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let "title"=document.querySelector("title");
+let "story-opening"=document.querySelector("story-opening");
+let "imagen12"=document.querySelector("imagen12");
+let "title"=document.querySelector("title");
+let "title"=document.querySelector("title");
+
+
+let "option-one" =document.querySelector("option-one");
+let "option-two" =document.querySelector("option-two")
+let
+
+option-one.onclick=function(){
+  paragraph.innerHTML=“Hello!”;
+};
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
